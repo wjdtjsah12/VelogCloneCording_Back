6e8ea2905993571a7b889cafc47a493dd894f339
Jung Hee <fkqm8293@naver.com>
2022-02-19 00:29:04 +0900

    no message

--- src/main/java/com/sparta/hanghare99_clonecording/model/User.java
+++ src/main/java/com/sparta/hanghare99_clonecording/model/User.java
@@ -1,33 +1,33 @@
-package com.sparta.hanghare99_clonecording.model;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-@Getter
-@NoArgsConstructor
-@Entity
-public class User {
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Id
-    private Long id;
-
-    @Column(nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private Long userId;
-
-    @Column(nullable = false)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false)
-    private String introduce;
-
-    @Column(nullable = false)
-    private Long profileNum;
-}
+//package com.sparta.hanghare99_clonecording.model;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//
+//@Getter
+//@NoArgsConstructor
+//@Entity
+//public class User {
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Id
+//    private Long id;
+//
+//    @Column(nullable = false)
+//    private String email;
+//
+//    @Column(nullable = false)
+//    private Long userId;
+//
+//    @Column(nullable = false)
+//    private String username;
+//
+//    @Column(nullable = false)
+//    private String password;
+//
+//    @Column(nullable = false)
+//    private String introduce;
+//
+//    @Column(nullable = false)
+//    private Long profileNum;
+//}
--- src/main/java/com/sparta/hanghare99_clonecording/repository/CommentRepository.java
+++ src/main/java/com/sparta/hanghare99_clonecording/repository/CommentRepository.java
@@ -1,7 +1,7 @@
-package com.sparta.hanghare99_clonecording.repository;
-
-import com.sparta.hanghare99_clonecording.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
+//package com.sparta.hanghare99_clonecording.repository;
+//
+//import com.sparta.hanghare99_clonecording.model.Comment;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface CommentRepository extends JpaRepository<Comment, Long> {
+//}
