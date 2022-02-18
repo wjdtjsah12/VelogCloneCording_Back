65028bb6e88553be8433327307ce22bc3595bda5
nerdved <wjdtjsah12@gmail.com>
2022-02-18 21:41:48 +0900

    Initial commit

---
+++ src/main/java/com/sparta/hanghare99_clonecording/Hanghare99ClonecordingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghare99_clonecording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghare99ClonecordingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hanghare99ClonecordingApplication.class, args);
+    }
+
+}
