0c4561f0ad6bb9fb55adc820d18ff3da09c2ada4
Jung Hee <fkqm8293@naver.com>
2022-02-22 10:33:19 +0900

    .....

--- src/main/java/com/sparta/hanghare99_clonecording/controller/BoardController.java
+++ src/main/java/com/sparta/hanghare99_clonecording/controller/BoardController.java
@@ -6,6 +6,7 @@ import com.sparta.hanghare99_clonecording.dto.BoardUpdateReponseDto;
 import com.sparta.hanghare99_clonecording.model.Board;
 import com.sparta.hanghare99_clonecording.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,18 +18,17 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/boards/likes")
-    public List<Board> getLikeBoards(){
+    public List<Board> getLikeBoards() {
         return boardService.getLikeBoards();
     }
 
     @PostMapping("/board/posting")
-    public BoardRegisterResponseDto postingBoard(@RequestBody BoardRegisterDto requestDto){
+    public BoardRegisterResponseDto postingBoard(@RequestBody BoardRegisterDto requestDto) {
         return boardService.postingBoard(requestDto);
     }
 
     @GetMapping("/board/detail/{postingId}")
     public Board readBoard(@PathVariable Long postingId) {
-
         return boardService.readBoard(postingId);
     }
 
@@ -42,4 +42,5 @@ public class BoardController {
     public void deleteBoard(@PathVariable Long postingId) {
         boardService.deleteBoard(postingId);
     }
+
 }
